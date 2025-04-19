cd8211554eafac4ecd3b1b120924fab98db2a62f
udayraj04 <sharmarajuday@gmail.com>
2025-04-19 20:01:11 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,6 @@ import companyRoute from "./routes/company.routes.js";
 import jobRoute from "./routes/job.routes.js";
 import applicationRoute from "./routes/application.routes.js";  
 
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -43,18 +41,6 @@ app.use("/resumeapplicant", userRoute);
 app.use("/talenttrack/company", companyRoute);
 app.use("/talenttrack/job", jobRoute);
 app.use("/talenttrack/application", applicationRoute);  
-
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static frontend
-app.use(express.static(path.join(__dirname, 'talenttrack/build'))); // adjust path if needed
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'talenttrack', 'build', 'index.html'));
-});
 
 
 
